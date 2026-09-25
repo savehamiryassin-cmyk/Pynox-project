e574e5fdfe076a98e5578a37b5332d16dda1755e
savehamiryassin-cmyk <savehamiryassin@gmail.com>
2026-09-25 16:18:46 +0330

    Delete Pynox old/Boot directory

--- Pynox old/Boot/bootloader.py
+++
@@ -1,8 +0,0 @@
-
-class Bootloader:
-    def __init__(self):
-        self.status = "Ready"
-    def load(self):
-        print("[BOOTLOADER] Loading Pynox...")
-        self.status = "Loaded"
-        print("[BOOTLOADER] Bootloader finished.")
